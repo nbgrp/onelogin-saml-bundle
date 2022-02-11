2bae9bbfeaa3fb9731f59615f54d3e20bf1bec87
Alexander Menshchikov <amenshchikov@gmail.com>
2022-02-11 12:34:40 +0300

    Missed SamlAuthenticationSuccessHandler service definition (after disabling autowire)

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -60,6 +60,12 @@ return static function (ContainerConfigurator $container): void {
 
         ->set(Onelogin\AuthRegistryInterface::class, Onelogin\AuthRegistry::class)
 
+        ->set(Security\Http\Authentication\SamlAuthenticationSuccessHandler::class)
+            ->args([
+                service(HttpUtils::class),
+                [], // Options
+            ])
+
         ->set(Security\Http\Authenticator\SamlAuthenticator::class)
             ->args([
                 /* 0 */ service(HttpUtils::class),
